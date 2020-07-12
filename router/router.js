f133b7139f16a2c7a81884315ebf7131220bcdab
rachOS <g.chamekh@e-nautia.com>
2020-07-12 16:24:04 +0200

    basic CRUD working on stack.js

--- router/router.js
+++ router/router.js
@@ -5,11 +5,13 @@ const router = express.Router();
 // import routes
 const developper = require("./developper");
 const project = require("./project");
+const stack = require("./stack");
 const team = require("./team");
 
 // init router
 router.use("/developpers", developper);
 router.use("/projects", project);
+router.use("/stacks", stack);
 router.use("/teams", team);
 
 // export router
---
+++ router/stack.js
@@ -0,0 +1,81 @@
+// import modules
+const express = require("express");
+const router = express.Router();
+
+// import connection
+const connection = require("../connection");
+const { route } = require("./project");
+
+/* CREATE */
+// Create one stack
+router.post("/", (req, res) => {
+    const formData = req.body;
+    const sql = "INSERT INTO stack SET ?";
+    connection.query(sql, [formData], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+            res.status(500).send("Impossible d'ajouter un langage");
+        } else {
+            res.status(201).send(
+                "Le langage de programmation a bien été ajouté"
+            );
+        }
+    });
+});
+
+/* READ */
+// Get all stacks
+router.get("/", (req, res) => {
+    const sql = "SELECT * FROM stack";
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send("Impossile d'afficher la liste des langages");
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
+
+// Get one stack
+router.get("/:idStack", (req, res) => {
+    const { idStack } = req.params;
+    const sql = "SELECT * FROM stack WHERE id = ?";
+    connection.query(sql, [idStack], (err, result) => {
+        if (err) {
+            res.status(200).send("Impossible d'afficher le langage");
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
+
+/* UPDATE */
+// Edit one stack
+router.put("/:idStack", (req, res) => {
+    const formData = req.body;
+    const { idStack } = req.params;
+    const sql = "UPDATE stack SET ? WHERE id = ?";
+    connection.query(sql, [formData, idStack], (err, result) => {
+        if (err) {
+            res.status(500).send("Impossible d'éditer le langage");
+        } else {
+            res.status(201).send("Le langage a bien été édité");
+        }
+    });
+});
+
+/* DELETE */
+// Delete one stack
+router.delete("/:idStack", (req, res) => {
+    const { idStack } = req.params;
+    const sql = "DELETE FROM stack WHERE id = ?";
+    connection.query(sql, [idStack], (err, result) => {
+        if (err) {
+            res.status(500).send("Impossible d'effacer le langage");
+        } else {
+            res.status(200).send("le langage a bien été effacé.");
+        }
+    });
+});
+
+module.exports = router;
